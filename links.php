3e40c5e1c93e322002e6b94328e027e866211d8f
Amit singh <230885788+amitsingh881024-ctrl@users.noreply.github.com>
2026-06-28 21:02:11 +0530

    Add links to CSS and JS files in links.php

---
+++ links.php
@@ -0,0 +1,24 @@
+
+<!-- vector map CSS -->
+<link href="vendors/vectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" type="text/css" />
+
+<!-- Toggles CSS -->
+<link href="vendors/jquery-toggles/css/toggles.css" rel="stylesheet" type="text/css">
+<link href="vendors/jquery-toggles/css/themes/toggles-light.css" rel="stylesheet" type="text/css">
+
+<!-- Toastr CSS -->
+<link href="vendors/jquery-toast-plugin/dist/jquery.toast.min.css" rel="stylesheet" type="text/css">
+
+<!-- Custom CSS -->
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<!--<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>-->
+<link rel="stylesheet" href="css/jquery.Jcrop.min.css" type="text/css" />
+<!--data table-->
+<link href="css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap4.min.js"></script>
+
+<script src="js/selectize.min.js" ></script>
+<link rel="stylesheet" href="css/selectize.bootstrap3.min.css" />
+<link rel="stylesheet" href="css/style_religare.css" />
