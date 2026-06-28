3405984747f962d82c35ef924696838f55f7eccb
Amit singh <230885788+amitsingh881024-ctrl@users.noreply.github.com>
2026-06-28 20:59:44 +0530

    Add initial invoice format HTML structure

---
+++ invoiceformat.php
@@ -0,0 +1,182 @@
+<html>
+<head>
+<title>Untitled Document</title>
+</head>
+<style>
+tr,
+td,
+th {
+font-size: 8px;
+}
+
+</style>
+<body >
+<table border="1" cellpadding="5" width="100%">
+    <tr>
+        <td><div>
+        <table width="100%" class="table">
+        <tr>
+            <td width="33%" valign="bottom" align="left">
+                <table>
+                    <tr>
+                        <td>
+                            <strong>Place of Supply: </strong>Uttar Pradesh
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>State Code: </strong>09
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Religare State GSTN: </strong><br>09AAHCR8454M1ZL
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td width="33%" valign="top" align="center">
+                <table>
+                    <tr>
+                        <td style="font-size:13px;">
+                            <strong>Religare Broking Ltd.</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Prius Global,2nd floor, Plot No-A-3 to A-5</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Sector-125,Noida-201301 (U.P)</strong>
+                        </td>
+                    </tr>
+    
+                </table>
+            </td>
+            <td width="33%" valign="top" align="right">
+                <table>
+                    <tr>
+                        <td>
+                            <img src="img/Religare-Logo.png" style="width:125px;" alt="logo" />
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table></div></td>
+    </tr>
+    <tr>
+        <td width="40%" valign="top">
+            <table>
+                <tr>
+                    <td>
+                        <div><br><strong>eMudhra Limite</strong><br></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div> <strong>Branch Code: </strong> 00001<br></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div> jIW AFSDUFHSFUIHS SDFKDSHFUDSH SDFUIDSHFIU<br></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div> <strong>GST No: </strong> 0JDFHDSJ4545640001<br></div>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <td width="60%" valign="top">
+            <table>
+                <tr>
+                    <td>
+                        <div><br><strong>IRN No: </strong>
+                            0f4767783e7d6d30b4ef39e6ef4c8b1c602f05585954bf2cd8d049e1dd959a94<br></div>
+                            
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><strong> IRN Date: </strong> 22-55-2222</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><strong> Invoice No: </strong> 22-55-2222</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><strong> Date: </strong> 22-55-2222</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><strong> Period: </strong> 22-55-2222 To 22-55-2222</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div><strong> Whether tax is payable on reverse charge : </strong> N</div>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<table border="1" cellpadding="10">
+    <tr align="center">
+        <th width="10%">S.No</th>
+        <th width="40%">Description</th>
+        <th width="10%">HSN Code</th>
+        <th width="10%">Qty</th>
+        <th width="15%">Rate</th>
+        <th width="15%">Amount</th>
+    </tr>
+    <tr>
+        <td align="center">1</td>
+        <td>CTRBLMUMBAI_commission on sale of digital signature for the period FY 21-22</td>
+        <td align="center">998319</td>
+        <td align="center">1</td>
+        <td align="center">5,279.50</td>
+        <td align="center">5,279.50</td>
+    </tr>
+    <tr>
+        <td colspan="4" align="right"><b>Total</b></td>
+        <td colspan="2" align="right">5,279.50</td>
+    </tr>
+    <tr>
+        <td colspan="4" align="right"><b>IGST @ 18%</b></td>
+        <td colspan="2" align="right">950.31</td>
+    </tr>
+    <tr>
+        <td colspan="4" align="right"><b>CGST@ 9%</b></td>
+        <td colspan="2" align="right"> 0.00</td>
+    </tr>
+    <tr>
+        <td colspan="4" align="right"><b>SGST@ 9%</b></td>
+        <td colspan="2" align="right"> 0.00</td>
+    </tr>
+</table>
+<table border="1" cellpadding="10">
+    <tr>
+        <td colspan="1" align="left"><b>Total Invoice Value(InWords):</b></td>
+        <td colspan="2" align="center"> Six Thousand Two Hundred Twenty-Nine And Eighty-One Paise Grand Total :
+            6,229.81Only</td>
+        <td colspan="1" align="center"><b>Grand Total: </b></td>
+        <td colspan="2" align="right">0.00</td>
+    </tr>
+</table>
+<table border="1" cellpadding="10">
+    <tr>
+        <td align="left">Terms & Conditions </br> E.& O.E</td>
+    </tr>
+ </table>
+</body>
+</html>
